626b3f68800e36d14e1de9b10dbe900405285126
MSKim0215 <rlaalstjqu05@gmail.com>
2024-05-28 01:47:39 +0900

    feat: add to swap block
    4. GameManager 클래스 구현
        - [x]  타일 선택 시 블록 이름 출력 구현
        - [x]  타일 스왑 기능 구현
            - [x]  마우스 위, 아래 드래그 이동
            - [x]  마우스 오른쪽, 왼쪽 드래그 이동

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.PlayerSettings;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,6 +11,13 @@ public class GameManager : MonoBehaviour
     private GameObject slotPrefab;
 
     private const int BOARD_SIZE = 10;
+
+    [SerializeField] private List<Slot> gameSlots = new List<Slot>();
+
+    [SerializeField] private Block grabBlock;
+    [SerializeField] private Block targetBlock;
+
+    private Vector2 startPos, endPos;
 
     private void Start()
     {
@@ -35,7 +43,83 @@ public class GameManager : MonoBehaviour
                 Vector2 spawnPos = new Vector2((float)j / 2 + 0.5f, (float)i / 2 + 0.5f);
                 GameObject spawnObj = Instantiate(slotPrefab, transform);
                 spawnObj.transform.localPosition = spawnPos;
+
+                gameSlots.Add(spawnObj.GetComponent<Slot>());  
             }
+        }
+    }
+
+    private Vector2 dragStartPos;
+
+    private void Update()
+    {
+        if(grabBlock == null && Input.GetMouseButtonDown(0))
+        {
+            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
+            if(hit.collider != null)
+            {
+                grabBlock = hit.collider.GetComponent<Block>();
+                startPos = grabBlock.transform.position;
+                dragStartPos = pos;
+            }
+        }
+
+        if(grabBlock != null)
+        {
+            Vector2 currPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            float dragDistance = Vector2.Distance(currPos, dragStartPos);
+
+            if(dragDistance > 0.3f)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(currPos, Vector2.zero, 0f);
+                if(hit.collider != null)
+                {
+                    targetBlock = hit.collider.GetComponent<Block>();
+
+                    Vector2 tempPos = targetBlock.transform.position;
+
+                    Vector2 dragDir = currPos - dragStartPos;
+                    float absXDiff = Mathf.Abs(dragDir.x);
+                    float absYDiff = Mathf.Abs(dragDir.y);
+
+                    if (absXDiff > absYDiff)
+                    {
+                        if (dragDir.x > 0)
+                        {
+                            Debug.Log("오른쪽 드래그");
+                        }
+                        else
+                        {
+                            Debug.Log("왼쪽 드래그");
+                        }
+                    }
+                    else
+                    {
+                        if (dragDir.y > 0)
+                        {
+                            Debug.Log("상단 드래그");
+                        }
+                        else
+                        {
+                            Debug.Log("하단 드래그");
+                        }
+                    }
+
+                    targetBlock.transform.position = grabBlock.transform.position;
+                    grabBlock.transform.position = tempPos;
+
+                    grabBlock = null;
+                    targetBlock = null;
+
+                    return;
+                }
+            }
+        }
+
+        if(grabBlock != null && Input.GetMouseButtonUp(0))
+        {
+            grabBlock = null;
         }
     }
 }
