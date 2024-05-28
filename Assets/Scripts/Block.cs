5d11c019257c8d0801b9134e3ea8bcbe7614bfdb
MSKim0215 <rlaalstjqu05@gmail.com>
2024-05-28 16:14:36 +0900

    refact: change to Drag Logic
    1. Block 클래스 구현
    - [x]  드래그 방향을 체크하여 어디로 이동할지 판단
        - 기존 GameManager 제거
            - Update() 함수에서 처리하는 방식이 생각보다 비용이 많이 발생한다고 판단했음
            - 그래서, 각 개인 블록에서 체크하는 방식으로 변경할 예정
                - OnMouseDown(), OnMouseUp() 활용

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,4 +6,46 @@ using UnityEngine.EventSystems;
 public class Block : MonoBehaviour
 {
     private Vector2 startPos, endPos;
+
+    private void OnMouseDown()
+    {
+        startPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseUp()
+    {
+        endPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        CalculateAngle();
+    }
+
+    private void CalculateAngle()
+    {
+        Vector2 dragDir = endPos - startPos;
+        float absXDiff = Mathf.Abs(dragDir.x);
+        float absYDiff = Mathf.Abs(dragDir.y);
+
+        if (absXDiff > absYDiff)
+        {
+            if (dragDir.x > 0)
+            {
+                Debug.Log("오른쪽 드래그");
+            }
+            else
+            {
+                Debug.Log("왼쪽 드래그");
+            }
+        }
+        else
+        {
+            if (dragDir.y > 0)
+            {
+                Debug.Log("상단 드래그");
+            }
+            else
+            {
+                Debug.Log("하단 드래그");
+            }
+        }
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -17,8 +17,6 @@ public class GameManager : MonoBehaviour
     [SerializeField] private Block grabBlock;
     [SerializeField] private Block targetBlock;
 
-    private Vector2 startPos, endPos;
-
     private void Start()
     {
         blockSprites = Resources.LoadAll<Sprite>("Sprites/Block");
@@ -46,80 +44,6 @@ public class GameManager : MonoBehaviour
 
                 gameSlots.Add(spawnObj.GetComponent<Slot>());  
             }
-        }
-    }
-
-    private Vector2 dragStartPos;
-
-    private void Update()
-    {
-        if(grabBlock == null && Input.GetMouseButtonDown(0))
-        {
-            Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 0f);
-            if(hit.collider != null)
-            {
-                grabBlock = hit.collider.GetComponent<Block>();
-                startPos = grabBlock.transform.position;
-                dragStartPos = pos;
-            }
-        }
-
-        if(grabBlock != null)
-        {
-            Vector2 currPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            float dragDistance = Vector2.Distance(currPos, dragStartPos);
-
-            if(dragDistance > 0.3f)
-            {
-                RaycastHit2D hit = Physics2D.Raycast(currPos, Vector2.zero, 0f);
-                if(hit.collider != null)
-                {
-                    targetBlock = hit.collider.GetComponent<Block>();
-
-                    Vector2 tempPos = targetBlock.transform.position;
-
-                    Vector2 dragDir = currPos - dragStartPos;
-                    float absXDiff = Mathf.Abs(dragDir.x);
-                    float absYDiff = Mathf.Abs(dragDir.y);
-
-                    if (absXDiff > absYDiff)
-                    {
-                        if (dragDir.x > 0)
-                        {
-                            Debug.Log("오른쪽 드래그");
-                        }
-                        else
-                        {
-                            Debug.Log("왼쪽 드래그");
-                        }
-                    }
-                    else
-                    {
-                        if (dragDir.y > 0)
-                        {
-                            Debug.Log("상단 드래그");
-                        }
-                        else
-                        {
-                            Debug.Log("하단 드래그");
-                        }
-                    }
-
-                    targetBlock.transform.position = grabBlock.transform.position;
-                    grabBlock.transform.position = tempPos;
-
-                    grabBlock = null;
-                    targetBlock = null;
-
-                    return;
-                }
-            }
-        }
-
-        if(grabBlock != null && Input.GetMouseButtonUp(0))
-        {
-            grabBlock = null;
         }
     }
 }
